9f9c756a33f08e276b25308f5b75c0f7c64f9048
allypetroova <allypetroovaa@gmail.com>
2024-03-18 17:45:09 +0300

    Started writing mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+// Mobile Nav
 
-});
+const mobileNavOpenBtn = document.querySelector('#open-mobile-nav-btn');
+const mobileNavCloseBtn = document.querySelector('#close-mobile-nav-btn');
+const mobileNav = document.querySelector('#mobile-nav');
+
+mobileNavOpenBtn.onclick = function (){
+  mobileNav.classList.add('mobile-nav-wrapper--open');
+}
+
+mobileNavCloseBtn.onclick = function () {
+  mobileNav.classList.remove('mobile-nav-wrapper--open');
+};
